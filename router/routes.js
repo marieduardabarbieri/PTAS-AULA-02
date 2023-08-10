bf17f308ae2b36a3f85c04e9a2917e574bf6b240
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2023-08-10 17:34:49 -0300

    colando as linhas de app.js para routes

--- router/routes.js
+++ router/routes.js
@@ -1,12 +1,19 @@
+const connectionDataBase = require('./config/connection');
 const express = require('express');
 const routes = express.Router()
-
+const routes = require('./router/routes')
 const userController = require('../controller/userController');
 
+const app = express();
+const port = 3003;
+app.use(express.json(), routes);
+app.listen(port, () => {console.log(`Run serve... ${port}`)})
+app.get('/', (req,res) => res.send("API-USER"));
+
 routes.post('/user', userController.createUser);
-
 routes.get('/user', userController.pegarDados);
-
 routes.delete('/user/:id', userController.deleteUser);
-
 routes.put('/user/:id', userController.updateUser);
+
+
+module.exports = routes;
